0e11f3cf3c5be6ebbc4a787b9ab3ccddc0157887
22peculiar <tommyjimmy619@gmail.com>
2024-01-31 12:30:10 +0300

    Added an image to the admin login page

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -9,6 +9,14 @@
 	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
 	<title>Admin Login Page</title>
+  
+  <style type="text/css">
+    .authlogin-side-wrapper{
+      width: 100%;
+      height: 100%;
+      background-image: url('/upload/login.png');
+    }
+  </style>
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -44,7 +52,7 @@
 						<div class="card">
 							<div class="row">
                 <div class="col-md-4 pe-md-0">
-                  <div class="auth-side-wrapper">
+                  <div class="authlogin-side-wrapper">
 
                   </div>
                 </div>
